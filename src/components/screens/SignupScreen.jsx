9474d25c720a23b2d5f29f4b77256b1af9297ccc
haoning-w <elwin.hn.wang@gmail.com>
2024-03-14 22:19:48 -0700

    Implemented SignupScreen

---
+++ src/components/screens/SignupScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+import CenterLayout from "../ui/CenterLayout";
+import AuthForm from "../features/auth/AuthForm";
+
+export default function SignupScreen() {
+  return (
+    <CenterLayout>
+      <AuthForm mode="signup" />
+    </CenterLayout>
+  );
+}
