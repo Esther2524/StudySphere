9460b367a8b85715237b874e338a7297aad5e088
haoning-w <elwin.hn.wang@gmail.com>
2024-03-15 22:07:05 -0700

    Implemented GroupCardItem

---
+++ src/components/features/studyGroup/GroupCardItem.jsx
@@ -0,0 +1,62 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import PressableButton from "../../ui/PressableButton";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Colors } from "../../../utils/Colors";
+
+export default function GroupCardItem({ title, numOfPeople }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <View style={styles.subLine}>
+        <View style={styles.infoContainer}>
+          <MaterialIcons name="group" size={20} color="black" />
+          <Text>{numOfPeople}</Text>
+        </View>
+        <PressableButton containerStyle={styles.joinBtnContainer}>
+          <Text style={styles.joinBtnText}>Join</Text>
+        </PressableButton>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 20,
+    paddingTop: 20,
+    paddingBottom: 12,
+    paddingHorizontal: 30,
+    backgroundColor: Colors.cardBgColor,
+    width: 360,
+    marginHorizontal: "auto",
+    borderRadius: 20,
+  },
+  subLine: {
+    flex: 1,
+    flexDirection: "row",
+    marginTop: 20,
+    alignItems: "center",
+  },
+  title: {
+    color: Colors.mainText,
+    fontSize: 20,
+  },
+  infoContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingTop: 12,
+    gap: 3,
+  },
+  joinBtnContainer: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: Colors.colorYellow,
+    borderRadius: 1000,
+  },
+  joinBtnText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
