9c95f3953f3d8fa53ba8814bff5ce11c7e358134
haoning-w <elwin.hn.wang@gmail.com>
2024-03-20 21:34:43 -0700

    Removed a redundant import

--- src/components/features/studyGroup/GroupCardList.jsx
+++ src/components/features/studyGroup/GroupCardList.jsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
 import React from "react";
 import GroupCardItem from "./GroupCardItem";
 
