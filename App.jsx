9e4bb768fc9484f84a612c052f2d0f263459f4e3
haoning-w <elwin.hn.wang@gmail.com>
2024-03-22 16:30:07 -0700

    Improve status bar's UI and added GluestackUIProvider

--- App.jsx
+++ App.jsx
@@ -2,16 +2,20 @@ import { StatusBar } from "expo-status-bar";
 import AuthStackNav from "./src/components/navigation/AuthStackNav";
 import AppTabNav from "./src/components/navigation/AppTabNav";
 import { NavigationContainer } from "@react-navigation/native";
-import { useState } from "react";
+import { useStore } from "./src/hooks/Store";
+import { GluestackUIProvider } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config";
 
 export default function App() {
-  const [isAuthed, setIsAuthed] = useState(false);
+  const isAuthed = useStore((state) => state.isAuthed);
 
   return (
-    <NavigationContainer>
-      {!isAuthed && <AuthStackNav />}
-      {isAuthed && <AppTabNav />}
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <GluestackUIProvider config={config}>
+      <NavigationContainer>
+        {!isAuthed && <AuthStackNav />}
+        {isAuthed && <AppTabNav />}
+        <StatusBar style="light" />
+      </NavigationContainer>
+    </GluestackUIProvider>
   );
 }
