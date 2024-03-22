5853d3a6f0171d0000df2a0efbafff26d89c2a0c
haoning-w <elwin.hn.wang@gmail.com>
2024-03-22 14:55:12 -0700

    Added functionality to set userId

--- src/hooks/Store.js
+++ src/hooks/Store.js
@@ -3,10 +3,15 @@ import { immer } from "zustand/middleware/immer";
 
 export const useStore = create()(
   immer((set) => ({
+    userId: "",
     isAuthed: false,
     setIsAuthed: (newState) =>
       set((state) => {
         state.isAuthed = newState;
+      }),
+    setCurrentUser: (id) =>
+      set((state) => {
+        state.userId = id;
       }),
   }))
 );
