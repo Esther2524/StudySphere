6578e0dcd27b7e5fb7f8d49c19d3f7ca838e30e8
haoning-w <elwin.hn.wang@gmail.com>
2024-03-24 15:56:56 -0700

    Implemented the functionality to create a group and store it in Firebase

--- src/components/features/studyGroup/AddGroupModal.jsx
+++ src/components/features/studyGroup/AddGroupModal.jsx
@@ -1,24 +1,57 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import React, { useCallback, useState } from "react";
 import ModalView from "../../ui/ModalView";
 import FormOperationBar from "../../ui/FormOperationBar";
 import InputWithLabel from "../../ui/InputWithLabel";
+import { getUserRef } from "../../../utils/helper";
+import { addDoc, collection } from "firebase/firestore";
+import { createGroupData } from "./studyGroupHelper";
+import { useNavigation } from "@react-navigation/native";
 
 export default function AddGroupModal({ setIsAddingGroup }) {
   const [groupName, setGroupName] = useState("");
   const [errMsg, setErrMsg] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const navigation = useNavigation();
+
+  const userRef = getUserRef();
+  const userGroupsRef = collection(userRef, "groups");
 
   const cancelHandler = useCallback(() => {
     setIsAddingGroup(false);
   }, [setIsAddingGroup]);
 
-  const confirmHandler = useCallback(() => {
+  const confirmHandler = useCallback(async () => {
     if (!groupName) {
       setErrMsg("Group name can't be empty!");
       return;
     }
-    setIsAddingGroup(false);
-  }, [groupName, setErrMsg, setIsAddingGroup]);
+    setIsSubmitting(true);
+
+    // Generate group document data
+    const newGroupData = createGroupData({
+      groupOwnerId: userRef.id,
+      groupName,
+    });
+
+    // Add the group to firebase
+    try {
+      const newGroupRef = await addDoc(userGroupsRef, newGroupData);
+      setIsAddingGroup(false);
+      navigation.navigate("Group Detail", {
+        groupName,
+        groupId: newGroupRef.id,
+      });
+    } catch (e) {
+      setIsSubmitting(false);
+      Alert.alert("Failed", e.message);
+    }
+  }, [groupName, createGroupData, userRef, userGroupsRef, navigation]);
+
+  const inputHandler = useCallback((newText) => {
+    setErrMsg("");
+    setGroupName(newText);
+  }, []);
 
   return (
     <ModalView isVisible>
@@ -27,7 +60,7 @@ export default function AddGroupModal({ setIsAddingGroup }) {
         <View style={{ alignItems: "center" }}>
           <InputWithLabel
             content={groupName}
-            setContent={setGroupName}
+            setContent={inputHandler}
             containerStyle={styles.input}
             placeholder="Enter Your Group's Name"
             errorMsg={errMsg}
@@ -39,6 +72,8 @@ export default function AddGroupModal({ setIsAddingGroup }) {
             confirmText="Confirm"
             confirmHandler={confirmHandler}
             cancelHandler={cancelHandler}
+            isSubmitting={isSubmitting}
+            confirmDisabled={isSubmitting}
           />
         </View>
       </View>
@@ -68,5 +103,6 @@ const styles = StyleSheet.create({
     width: 300,
     height: 80,
     marginBottom: 20,
+    marginTop: -10,
   },
 });
