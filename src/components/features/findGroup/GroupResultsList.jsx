3892e56ed60bb479b41f7858fd8a78ac882338ae
haoning-w <elwin.hn.wang@gmail.com>
2024-03-25 15:10:58 -0700

    Replace temp local data with actual data from Firebase

--- src/components/features/findGroup/GroupResultsList.jsx
+++ src/components/features/findGroup/GroupResultsList.jsx
@@ -1,27 +1,37 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import GroupResultsItem from "./GroupResultsItem";
 import { Colors } from "../../../utils/Colors";
-
-const tempData = [
-  { groupName: "Study React Native Together!", numOfPeople: 30, groupId: 1 },
-  { groupName: "We love reading :)", numOfPeople: 12, groupId: 2 },
-];
+import { useEffect, useState } from "react";
+import { searchGroup } from "./findGroupHelper";
 
 export default function GroupResultsList({ keyword }) {
-  const results = tempData.filter((item) =>
-    item.groupName.toLowerCase().includes(keyword.toLowerCase())
-  );
+  const [groupData, setGroupData] = useState([]);
+
+  useEffect(() => {
+    const handlerSearch = async () => {
+      const data = await searchGroup(keyword);
+      setGroupData(data);
+    };
+    if (keyword) handlerSearch();
+    else setGroupData([]);
+  }, [keyword, searchGroup]);
 
   return (
     <View style={styles.container}>
-      {results.length > 0 && keyword && (
-        <Text style={styles.listTitle}>Results</Text>
+      {groupData.length > 0 && <Text style={styles.listTitle}>Results</Text>}
+      {groupData.length > 0 && (
+        <FlatList
+          data={groupData}
+          renderItem={({ item }) => (
+            <GroupResultsItem
+              groupId={item.groupId}
+              groupName={item.groupName}
+              groupSize={item.groupSize}
+            />
+          )}
+          style={{ marginBottom: 150 }}
+        />
       )}
-      {keyword &&
-        results.map((item) => (
-          <GroupResultsItem {...item} key={item.groupId} />
-        ))}
     </View>
   );
 }
