fa50a1d1986b808d0ce3102b939c26fa16b3b17a
Esther2524 <wangzhixiao2020@163.com>
2024-03-24 21:23:13 -0700

    Added the add new focus button next to the header

--- src/components/screens/FocusScreen.jsx
+++ src/components/screens/FocusScreen.jsx
@@ -1,11 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import React, { useEffect, useState, useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { getDocs, collection, query, where } from 'firebase/firestore';
 import { Colors } from '../../utils/Colors';
+import { db, auth } from '../../api/FirestoreConfig';
+import FocusCard from '../features/focusList/FocusCard';
+import AddFocus from '../features/focusList/AddFocus';
+import { AntDesign } from '@expo/vector-icons';
+import PressableButton from '../ui/PressableButton';
+
 
 export default function FocusScreen() {
+
+  const [focusTasks, setFocusTasks] = useState([]);
+  const [isAddFocusVisible, setIsAddFocusVisible] = useState(false);
+
+  // use navigation dynamically set the navigation options, including adding a button to the screen's header
+  const navigation = useNavigation();
+
+
+  // use useLayoutEffect to set the navigation options and include the button in the header
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <PressableButton onPress={() => setIsAddFocusVisible(true)}>
+          <AntDesign name="pluscircleo" size={24} color={Colors.addFocusButton} />
+        </PressableButton>
+      ),
+    });
+  }, [navigation]);
+
+
+  useEffect(() => {
+    const fetchFocusTasks = async () => {
+      // get the current user info using `auth`
+      const user = auth.currentUser;
+      if (user) {
+        try {
+          const q = query(collection(db, "users", user.uid, "focus"));
+          const querySnapshot = await getDocs(q);
+          const tasks = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+          setFocusTasks(tasks);
+        } catch (error) {
+          console.log("Error fetching focus tasks:", error);
+        }
+      } else {
+        console.log("No user is currently signed in.");
+      }
+    };
+
+    fetchFocusTasks();
+  }, []);
+
+
+
   return (
     <View style={styles.container}>
-      <Text>FocusScreen</Text>
+      <FlatList
+        data={focusTasks}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <FocusCard />
+        )}
+      />
+      <AddFocus
+        isAddFocusVisible={isAddFocusVisible}
+        setIsAddFocusVisible={setIsAddFocusVisible}
+      />
     </View>
   )
 }
@@ -14,5 +75,9 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.screenBgColor,
     flex: 1,
+  },
+  focusLabel: {
+    color: Colors.focusText,
+    fontSize: 16,
   },
 })
