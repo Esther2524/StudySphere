6039eb435d4cf711bae2c8945b875e1158be09a3
haoning-w <elwin.hn.wang@gmail.com>
2024-03-24 20:35:43 -0700

    Implemented the api to directly add a group to Firebase. Also fixed the bug of getAllGroupsByUser when a user doesn't have any group

--- src/components/features/studyGroup/studyGroupHelper.js
+++ src/components/features/studyGroup/studyGroupHelper.js
@@ -1,16 +1,18 @@
 import {
+  addDoc,
   collection,
   doc,
   documentId,
   getDoc,
   getDocs,
   query,
+  updateDoc,
   where,
 } from "firebase/firestore";
 import { db } from "../../../api/FirestoreConfig";
 import { getUserRef } from "../../../utils/helper";
 
-export function createGroupData({ groupOwnerId, groupName }) {
+function createGroupData({ groupOwnerId, groupName }) {
   return {
     groupOwnerId,
     groupName,
@@ -24,6 +26,8 @@ export async function getAllGroupsByUser() {
   const groupsRef = collection(db, "groups");
   const userRes = await getDoc(userRef);
   const userGroupIds = userRes.data().groups.map((item) => item.groupId);
+
+  if (userGroupIds.length === 0) return data;
 
   const q = query(groupsRef, where(documentId(), "in", userGroupIds));
   const querySnapshot = await getDocs(q);
@@ -43,4 +47,27 @@ export async function getGroupDetail(groupId) {
   const docRef = doc(db, "groups", groupId);
   const res = await getDoc(docRef);
   return res.data();
+}
+
+export async function addGroupApi(groupName) {
+  const userRef = getUserRef();
+  const groupRef = collection(db, "groups");
+
+  // Generate group document data
+  const newGroupData = createGroupData({
+    groupOwnerId: userRef.id,
+    groupName,
+  });
+
+  // Add new group to group collection
+  const newGroupRef = await addDoc(groupRef, newGroupData);
+
+  // Add groupId to user's groups field
+  const userData = await getDoc(userRef);
+  const curGroups = userData.data().groups;
+  curGroups.push({ groupId: newGroupRef.id, joined: true });
+  await updateDoc(userRef, {
+    groups: curGroups,
+  });
+  return { ...newGroupData, groupId: newGroupRef.id };
 }
