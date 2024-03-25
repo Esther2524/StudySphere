dba259c9b138c39460e09b6cae540abd791fdd86
haoning-w <elwin.hn.wang@gmail.com>
2024-03-24 23:44:04 -0700

    Bug fix

--- src/components/features/studyGroup/useGetGroupDetail.js
+++ src/components/features/studyGroup/useGetGroupDetail.js
@@ -2,12 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import { getGroupDetail } from "./studyGroupHelper";
 
 export default function useGetGroupDetail(groupId) {
-  const { data, isLoading, error } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: [groupId, "group-detail"],
     queryFn: () => getGroupDetail(groupId),
-    throwOnError: (err) => {
-      console.log(err);
-    },
   });
-  return { data, isLoading, error };
+  return { data, isLoading };
 }
