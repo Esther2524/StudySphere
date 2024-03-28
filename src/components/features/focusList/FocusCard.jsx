441a1d91eb60afbc7f88cc906dd67fffb1513063
Esther2524 <wangzhixiao2020@163.com>
2024-03-27 21:09:32 -0700

    Adjusted the length of focus' title, and added an icon and number to display todayTimes

--- src/components/features/focusList/FocusCard.jsx
+++ src/components/features/focusList/FocusCard.jsx
@@ -2,24 +2,40 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import { Colors } from '../../../utils/Colors';
 import PressableButton from '../../ui/PressableButton';
+import { limitStrLen } from '../../../utils/helper';
+import { AntDesign } from '@expo/vector-icons';
 
-export default function FocusCard({ title, duration, onStartPress, onEditPress }) {
+export default function FocusCard({ title, duration, todayTimes, onStartPress, onEditPress }) {
 
-
+  
   return (
     <PressableButton
       onPress={onEditPress}
     >
       <View style={styles.card}>
         <View style={styles.cardText}>
-          <Text style={styles.title}>{title}</Text>
-          <Text style={styles.duration}>{duration} min</Text>
+          <Text style={styles.title}>{limitStrLen(title, 20)}</Text>
+          <View style={styles.secondLine}>
+            <Text style={styles.duration}>{duration} min</Text>
+            {
+              todayTimes >= 1 ? (
+                <>
+                  <AntDesign style={styles.icon} name="checkcircle" size={24} color={Colors.deleteButton} />
+                  <Text style={styles.studyTimes}>{todayTimes}</Text>
+                </>
+              ) : (
+                <AntDesign style={styles.icon} name="checkcircleo" size={24} color={Colors.deleteButton} />
+              )
+            }
+
+          </View>
+
         </View>
-          <PressableButton onPress={onStartPress} containerStyle={styles.startButton}>
-            <Text style={styles.startText}>Start</Text>
-          </PressableButton>
+        <PressableButton onPress={onStartPress} containerStyle={styles.startButton}>
+          <Text style={styles.startText}>Start</Text>
+        </PressableButton>
       </View>
-    </PressableButton>
+    </PressableButton >
 
   )
 }
@@ -58,12 +74,27 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: Colors.title,
   },
+  pressed: {
+    opacity: 0.5,
+  },
+  secondLine: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   duration: {
     fontSize: 16,
     color: Colors.text,
     marginTop: 8,
+    marginRight: 12,
   },
-  pressed: {
-    opacity: 0.5,
+  icon: {
+    marginTop: 8,
   },
+  studyTimes: {
+    fontSize: 15,
+    marginTop: 9,
+    marginLeft: 3,
+    color: Colors.greyIconColor,
+  }
+
 })
