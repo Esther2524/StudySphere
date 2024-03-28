2d507c3d39df635a6e06ec29bad16d438827d2a5
haoning-w <elwin.hn.wang@gmail.com>
2024-03-28 14:37:23 -0700

    Added refresh control

--- src/components/screens/DashboardScreen.jsx
+++ src/components/screens/DashboardScreen.jsx
@@ -1,15 +1,35 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { RefreshControl, ScrollView, StyleSheet } from "react-native";
+import React, { useState } from "react";
 import { Colors } from "../../utils/Colors";
 import DailyOverview from "../features/dashboard/DailyOverview";
 import TaskBreakdown from "../features/dashboard/TaskBreakdown";
 import FocusTrend from "../features/dashboard/FocusTrend";
+import { useQueryClient } from "@tanstack/react-query";
 
 export default function DashboardScreen() {
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const queryClient = useQueryClient();
+
+  const onRefresh = () => {
+    setIsRefreshing(true);
+    queryClient.invalidateQueries(["trend-data"]);
+    queryClient.invalidateQueries(["today-data"]);
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 2000);
+  };
+
   return (
     <ScrollView
       style={styles.container}
       contentContainerStyle={styles.contentContainerStyle}
+      refreshControl={
+        <RefreshControl
+          refreshing={isRefreshing}
+          onRefresh={onRefresh}
+          tintColor={Colors.headerTitleColor}
+        />
+      }
     >
       <DailyOverview />
       <TaskBreakdown />
