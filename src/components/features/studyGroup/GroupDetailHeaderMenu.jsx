3284bf8b0ee72b77442f6c054e99c691ec4b43ca
haoning-w <elwin.hn.wang@gmail.com>
2024-04-01 20:58:32 -0700

    Replaced Gluestack Menu

--- src/components/features/studyGroup/GroupDetailHeaderMenu.jsx
+++ src/components/features/studyGroup/GroupDetailHeaderMenu.jsx
@@ -1,15 +1,39 @@
-import { Alert, StyleSheet } from "react-native";
-import React from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useState } from "react";
 import { Entypo } from "@expo/vector-icons";
 import { Colors } from "../../../utils/Colors";
-import { Button, Menu, MenuItem, MenuItemLabel } from "@gluestack-ui/themed";
 import { AntDesign } from "@expo/vector-icons";
 import { getUserRef } from "../../../utils/helper";
 import useQuitGroup from "./useQuitGroup";
 import { useNavigation } from "@react-navigation/native";
+import { Overlay } from "@rneui/themed";
+import PressableButton from "../../ui/PressableButton";
+
+function Menu({ toggleMenu, triggerAlert }) {
+  const quitOption = "Quit Group";
+  return (
+    <Overlay
+      backdropStyle={styles.menuBackdrop}
+      fullScreen={false}
+      overlayStyle={styles.menu}
+      onBackdropPress={toggleMenu}
+    >
+      <PressableButton containerStyle={styles.menuLine} onPress={triggerAlert}>
+        <AntDesign
+          name="closecircleo"
+          size={24}
+          color="black"
+          style={styles.menuIcon}
+        />
+        <Text style={styles.menuText}>{quitOption}</Text>
+      </PressableButton>
+    </Overlay>
+  );
+}
 
 export default function GroupDetailHeaderMenu({ groupId, groupOwnerId }) {
   const navigation = useNavigation();
+  const [showMenu, setShowMenu] = useState(false);
   const userRef = getUserRef();
   const curUserId = userRef.id;
   const isOwner = curUserId === groupOwnerId;
@@ -19,8 +43,6 @@ export default function GroupDetailHeaderMenu({ groupId, groupOwnerId }) {
     quitGroup(groupId);
     navigation.goBack();
   };
-
-  const quitOption = "Quit Group";
 
   const alertMsg = isOwner
     ? "You are the owner of the group. Once quit, the group will be deleted. Are you sure to quit?"
@@ -33,51 +55,48 @@ export default function GroupDetailHeaderMenu({ groupId, groupOwnerId }) {
     ]);
   };
 
+  const toggleMenu = useCallback(() => {
+    setShowMenu((show) => !show);
+  }, []);
+
   return (
-    <Menu
-      placement="bottom right"
-      trigger={({ ...triggerProps }) => (
-        <Button {...triggerProps} style={{ backgroundColor: "transparent" }}>
-          <Entypo
-            name="dots-three-horizontal"
-            size={24}
-            color={Colors.headerTitleColor}
-          />
-        </Button>
-      )}
-      style={styles.menu}
-      offset={5}
-    >
-      <MenuItem
-        key={quitOption}
-        textValue={quitOption}
-        style={styles.menuItemContainer}
-        onPress={triggerAlert}
-      >
-        <AntDesign
-          name="closecircleo"
+    <>
+      <PressableButton onPress={toggleMenu}>
+        <Entypo
+          name="dots-three-horizontal"
           size={24}
-          color="black"
-          style={styles.closeIcon}
+          color={Colors.headerTitleColor}
         />
-        <MenuItemLabel>{quitOption}</MenuItemLabel>
-      </MenuItem>
-    </Menu>
+      </PressableButton>
+      {showMenu && <Menu toggleMenu={toggleMenu} triggerAlert={triggerAlert} />}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  menuBackdrop: {
+    backgroundColor: "transparent",
+  },
   menu: {
-    minWidth: 180,
+    width: 180,
+    height: 60,
+    justifyContent: "center",
+    top: 100,
+    right: 10,
+    borderRadius: 10,
+    position: "absolute",
     shadowColor: "black",
     shadowRadius: 30,
     shadowOffset: { width: -20, height: 10 },
     shadowOpacity: 0.2,
   },
-  menuItemContainer: {
-    width: 180,
-  },
-  closeIcon: {
+  menuIcon: {
     marginHorizontal: 10,
+  },
+  menuText: { fontSize: 16 },
+
+  menuLine: {
+    flexDirection: "row",
+    alignItems: "center",
   },
 });
