e16c57f733baea0f208445a85e6fd553b2a384cf
haoning-w <elwin.hn.wang@gmail.com>
2024-04-01 17:46:50 -0700

    Added placeholder when no content is available

--- src/components/features/studyGroup/GroupCardList.jsx
+++ src/components/features/studyGroup/GroupCardList.jsx
@@ -1,9 +1,10 @@
-import { View, FlatList, StyleSheet, RefreshControl } from "react-native";
+import { View, FlatList, StyleSheet, RefreshControl, Text } from "react-native";
 import React from "react";
 import GroupCardItem from "./GroupCardItem";
 import useGetAllGroupsByUser from "./useGetAllGroupsByUser";
 import { useQueryClient } from "@tanstack/react-query";
 import { Colors } from "../../../utils/Colors";
+import LottieView from "lottie-react-native";
 
 export default function groupCardList() {
   const queryClient = useQueryClient();
@@ -19,7 +20,7 @@ export default function groupCardList() {
         Array.from({ length: 3 }).map((_, index) => (
           <GroupCardItem key={index} isLoading={true} />
         ))}
-      {!isLoading && (
+      {!isLoading && groupData.length >= 0 && (
         <FlatList
           contentContainerStyle={{ paddingBottom: 100 }}
           data={groupData}
@@ -42,6 +43,20 @@ export default function groupCardList() {
           )}
         />
       )}
+      {!isLoading && groupData.length === 0 && (
+        <View style={styles.placeholderContainer}>
+          <LottieView
+            style={{ width: "100%", height: "100%" }}
+            source={require("../../../../assets/placeholder-StudyGroup.json")}
+            autoPlay
+            loop
+          />
+          <Text style={styles.placeholderText}>
+            You haven't joined any group yet. Create your group or search a
+            group to join.
+          </Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -51,5 +66,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     marginTop: 20,
+  },
+  placeholderContainer: {
+    width: 250,
+    height: 250,
+    marginBottom: 350,
+    borderRadius: 20,
+  },
+  placeholderText: {
+    fontSize: 16,
+    color: Colors.shallowTextColor,
+    marginTop: -20,
   },
 });
