d6453fd56d7667ef4817ab8436a16f5b534c62bf
haoning-w <elwin.hn.wang@gmail.com>
2024-04-04 17:53:32 -0700

    Used variables to manage reminder-related constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
+// Screen title, nav name
 export const GROUP_SCREEN_TITLE = "Study Group";
 export const FIND_SCREEN_TITLE = "Find Group";
 export const FOCUS_SCREEN_TITLE = "Focus Tasks";
@@ -7,3 +8,11 @@ export const GROUP_STACK_NAME = "Group Nav";
 export const FOCUS_STACK_NAME = "Focus List";
 export const GROUP_DETAIL_SCREEN_TITLE = "Group Detail";
 export const STANDBY_SCREEN_NAME = "Standby";
+
+// constants about user-set reminder
+export const REMINDER_TYPE_DAILY = "Daily";
+export const REMINDER_TYPE_WEEKDAY = "Weekdays";
+export const REMINDER_TYPE_WEEKEND = "Weekends";
+export const REMINDER_TITLE = "Time to focus!";
+export const REMINDER_MSG =
+  "Ready to hit your study goals? Let's dive into a focused session and make today productive!";
