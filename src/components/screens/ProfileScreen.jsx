3aec9ec5b7421ef35ee53cb480794e0210746f73
Esther2524 <wangzhixiao2020@163.com>
2024-04-05 21:18:04 -0700

    Added a map on Profile screen to display all focus tasks' locations

--- src/components/screens/ProfileScreen.jsx
+++ src/components/screens/ProfileScreen.jsx
@@ -2,15 +2,19 @@ import { StyleSheet, Text, View, TextInput, Image } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { Colors } from '../../utils/Colors';
 import PressableButton from '../ui/PressableButton';
-import ModalView from '../ui/ModalView';
-import FormOperationBar from '../ui/FormOperationBar';
-import InputWithLabel from '../ui/InputWithLabel';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { auth, db } from '../../api/FirestoreConfig';
-import { getDoc, doc, updateDoc } from 'firebase/firestore';
+import { getDoc, doc, updateDoc, query, collection, getDocs, onSnapshot } from 'firebase/firestore';
+import EditNameModal from '../features/userProfile/EditNameModal';
+import MapView, { Marker } from 'react-native-maps';
+import locateFocusHandler from '../features/focusList/LocationHelper';
 
 
 const defaultAvatar = require('../../../assets/defaultAvatar.jpg');
+const DEFAULT_CENTER = {
+  latitude: 49.22199308411145,
+  longitude: -122.95789036911256,
+}
 
 export default function ProfileScreen() {
 
@@ -22,6 +26,17 @@ export default function ProfileScreen() {
   });
   const [editModal, setEditModal] = useState(false);
   const [newUserName, setNewUserName] = useState('');
+
+  // for display locations
+  const [focusTasksLocations, setFocusTasksLocations] = useState([]);
+  const [currLocation, setCurrLocation] = useState([]);
+  const [mapRegion, setMapRegion] = useState({
+    latitude: DEFAULT_CENTER.latitude,
+    longitude: DEFAULT_CENTER.longitude,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+  
 
 
   useEffect(() => {
@@ -37,11 +52,51 @@ export default function ProfileScreen() {
         }
       }
     });
-
     return () => unsubscribe(); // cleanup subscription on unmount
   }, []);
 
 
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (user) {
+      const tasksQuery = query(collection(db, "users", user.uid, "focus"));
+      // use onSnapshot here to listen for realtime updates from Firestore for the focus tasks
+      // then any change in the focus tasks collection for the user triggers an automatic update in this component
+      // if I edit/add a focus, the map should be updated right away
+      const unsubscribe = onSnapshot(tasksQuery, (querySnapshot) => {
+        const tasksLocations = querySnapshot.docs
+          .filter(doc => doc.data().location)
+          .map(doc => ({
+            ...doc.data().location,
+            key: doc.id,
+          }));
+        setFocusTasksLocations(tasksLocations);
+
+        const fetchLocation = async () => {
+          const location = await locateFocusHandler();
+          setCurrLocation(location);
+        };
+        fetchLocation().catch(console.error);
+   
+      });
+  
+      return () => unsubscribe();
+    }
+  }, []);
+
+
+  // update mapRegion (passed to MapView to be the center) 
+  // only when currLocation changes (when database changes and then fetching the device's current location)
+  useEffect(() => {
+    if (currLocation) {
+      setMapRegion({
+        ...mapRegion,
+        latitude: currLocation.latitude,
+        longitude: currLocation.longitude,
+      });
+    }
+  }, [currLocation]);
+  
 
   const handleSignOut = async () => {
     try {
@@ -81,24 +136,30 @@ export default function ProfileScreen() {
         <Text style={styles.logoutButtonText}> Log Out</Text>
       </PressableButton>
 
-      <ModalView isVisible={editModal}>
-        <View style={styles.modalView}>
-          <Text style={styles.title}>Edit Username</Text>
-          <InputWithLabel
-            content={newUserName}
-            setContent={setNewUserName}
-            placeholder="Enter your new username"
-            containerStyle={{ width: '100%', marginTop: 20 }}
+      <MapView
+        provider='google'
+        style={styles.mapStyle}
+        region={mapRegion}
+      >
+        {focusTasksLocations.map(loc => (
+          <Marker
+            key={loc.key}
+            coordinate={{
+              latitude: loc.latitude,
+              longitude: loc.longitude
+            }}
           />
-          <FormOperationBar
-            confirmText="Save"
-            cancelText="Cancel"
-            confirmHandler={handleEditUserName}
-            cancelHandler={() => { setEditModal(false) }}
-          />
-        </View>
+        ))}
+      </MapView>
 
-      </ModalView>
+      <EditNameModal
+        editModal={editModal}
+        setEditModal={setEditModal}
+        newUserName={newUserName}
+        setNewUserName={setNewUserName}
+        handleEditUserName={handleEditUserName}
+      />
+
     </View>
   )
 }
@@ -156,10 +217,13 @@ const styles = StyleSheet.create({
     color: Colors.mainText,
     textAlign: 'center',
   },
-  modalView: {
-    backgroundColor: 'white',
-    padding: 20,
+  image: {
+    width: '80%', // Adjust as necessary
+    height: 400,
     borderRadius: 20,
-    width: '80%',
   },
+  mapStyle: {
+    width: 400,
+    height: 400,
+  }
 })
