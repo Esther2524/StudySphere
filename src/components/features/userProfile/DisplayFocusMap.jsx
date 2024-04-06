abc19225d47cbd171c2d22dc44312bd794c1937f
Esther2524 <wangzhixiao2020@163.com>
2024-04-06 11:22:37 -0700

    Adjusted the size of the map on Profile screen

--- src/components/features/userProfile/DisplayFocusMap.jsx
+++ src/components/features/userProfile/DisplayFocusMap.jsx
@@ -1,33 +1,64 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import MapView, { Marker } from 'react-native-maps';
+import { Colors } from '../../../utils/Colors';
+import PressableButton from '../../ui/PressableButton';
 
-export default function DisplayFocusMap({ focusTasksLocations, mapRegion }) {
+export default function DisplayFocusMap({
+  focusTasksLocations, mapRegion, isMapShown, setIsMapShown
+}) {
+  // console.log(focusTasksLocations);
   return (
-    <MapView
-      provider='google'
-      style={styles.mapStyle}
-      region={mapRegion}
-    >
-      {focusTasksLocations.map(loc => (
-        <Marker
-          key={loc.key}
-          coordinate={{
-            latitude: loc.latitude,
-            longitude: loc.longitude
-          }}
-        />
-      ))}
-    </MapView>
+    <View style={styles.container}>
+      <PressableButton
+        onPress={() => setIsMapShown(!isMapShown)}
+        containerStyle={styles.showMapButton}>
+        <Text style={styles.showMapButtonText}>{isMapShown ? "Hide All Focus" : "Show All Focus"}</Text>
+      </PressableButton>
 
+      {isMapShown
+        && <MapView
+          provider='google'
+          style={styles.mapStyle}
+          region={mapRegion}
+        >
+          {focusTasksLocations.map(loc => (
+            <Marker
+              key={loc.key}
+              coordinate={{
+                latitude: loc.latitude,
+                longitude: loc.longitude
+              }}
+            />
+          ))}
+        </MapView>
+      }
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
+  container: {
+    height: 450,
+    width: 400,
+    alignItems: 'center',
+  },
   mapStyle: {
-    width: '80%',
-    height: '40%',
+    width: '85%',
+    height: '70%',
     borderRadius: 20,
     marginTop: 20,
-  }
+  },
+  showMapButton: {
+    marginTop: 30,
+    backgroundColor: Colors.showMapButtonBg,
+    borderRadius: 10,
+    padding: 10,
+    width: 150,
+    height: 40,
+    alignItems: 'center',
+  },
+  showMapButtonText: {
+    fontSize: 16,
+  },
 })
