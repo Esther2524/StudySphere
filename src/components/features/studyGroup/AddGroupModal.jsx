59a32cde52345f8597a8e6bb0e7e23b1bd499adc
haoning-w <elwin.hn.wang@gmail.com>
2024-04-07 11:45:02 -0700

    Implemented the UI to set group target time

--- src/components/features/studyGroup/AddGroupModal.jsx
+++ src/components/features/studyGroup/AddGroupModal.jsx
@@ -9,7 +9,11 @@ import { GROUP_DETAIL_SCREEN_TITLE } from "../../../utils/constants";
 
 export default function AddGroupModal({ setShowAddGroupModal }) {
   const [groupName, setGroupName] = useState("");
-  const [errMsg, setErrMsg] = useState("");
+  const [groupTarget, setGroupTarget] = useState("");
+  const [errMsg, setErrMsg] = useState({
+    nameErr: "",
+    targetErr: "",
+  });
   const navigation = useNavigation();
 
   const cancelHandler = () => setShowAddGroupModal(false);
@@ -32,16 +36,43 @@ export default function AddGroupModal({ setShowAddGroupModal }) {
   });
 
   const confirmHandler = useCallback(async () => {
+    let isValid = true;
     if (!groupName) {
-      setErrMsg("Group name can't be empty!");
-      return;
+      setErrMsg((pre) => ({
+        ...pre,
+        nameErr: "Group name can't be empty!",
+      }));
+      isValid = false;
     }
-    addGroup(groupName);
-  }, [groupName, addGroup]);
+    if (isNaN(groupTarget) || !groupTarget) {
+      setErrMsg((pre) => ({
+        ...pre,
+        targetErr: "Target must be a number!",
+      }));
+      isValid = false;
+    }
+    if (!isNaN(groupTarget) && groupTarget) {
+      if (Number(groupTarget) > 24 || Number(groupTarget) < 0) {
+        setErrMsg((pre) => ({
+          ...pre,
+          targetErr: "Target must be between 0 and 24!",
+        }));
+        isValid = false;
+      }
+    }
 
-  const inputHandler = useCallback((newText) => {
-    setErrMsg("");
+    if (!isValid) return;
+    addGroup(groupName);
+  }, [groupName, addGroup, groupTarget]);
+
+  const setNameHandler = useCallback((newText) => {
+    setErrMsg((pre) => ({ ...pre, nameErr: "" }));
     setGroupName(newText);
+  }, []);
+
+  const setTargetHandler = useCallback((newTarget) => {
+    setErrMsg((pre) => ({ ...pre, targetErr: "" }));
+    setGroupTarget(newTarget);
   }, []);
 
   return (
@@ -50,11 +81,21 @@ export default function AddGroupModal({ setShowAddGroupModal }) {
         <Text style={styles.title}>Create New Group</Text>
         <View style={{ alignItems: "center" }}>
           <InputWithLabel
+            label="Group Name *"
             content={groupName}
-            setContent={inputHandler}
+            setContent={setNameHandler}
             containerStyle={styles.input}
             placeholder="Enter Your Group's Name"
-            errorMsg={errMsg}
+            errorMsg={errMsg.nameErr}
+          />
+          <InputWithLabel
+            label="Target Hours *"
+            content={groupTarget}
+            setContent={setTargetHandler}
+            containerStyle={styles.input}
+            placeholder="Set target hours"
+            keyboardType="numeric"
+            errorMsg={errMsg.targetErr}
           />
         </View>
         <View style={styles.btnGroup}>
@@ -83,6 +124,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     width: "100%",
     justifyContent: "space-around",
+    marginTop: 10,
   },
   title: {
     textAlign: "center",
@@ -91,7 +133,7 @@ const styles = StyleSheet.create({
   },
   input: {
     width: 300,
-    height: 80,
-    marginBottom: 20,
+    height: 100,
+    marginBottom: 10,
   },
 });
