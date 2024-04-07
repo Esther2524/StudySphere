4cd45e80ac69a3011b5793d0d644710078a7d158
haoning-w <elwin.hn.wang@gmail.com>
2024-04-07 14:12:50 -0700

    Adjusted addGroup hook according to api change

--- src/components/features/studyGroup/useAddGroup.js
+++ src/components/features/studyGroup/useAddGroup.js
@@ -4,11 +4,18 @@ import { addGroupApi } from "./studyGroupHelper";
 export default function useAddGroup({ onSuccess, onError }) {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
-    mutationFn: (groupName, groupTarget) => addGroupApi(groupName, groupTarget),
+    mutationFn: ({ groupName, groupTarget }) =>
+      addGroupApi(groupName, groupTarget),
     onSuccess: (data) => {
-      const { groupName, groupId, groupOwnerId } = data;
+      const { groupName, groupId, groupOwnerId, groupTarget } = data;
       queryClient.setQueryData(["groups"], (cache) => {
-        cache.push({ groupName, groupId, groupOwnerId, groupSize: 1 });
+        cache.push({
+          groupName,
+          groupId,
+          groupOwnerId,
+          groupSize: 1,
+          groupTarget,
+        });
       });
       onSuccess({ groupName, groupId });
     },
