c882889e6e68f02d6c1e09d58783917c13ee231b
haoning-w <elwin.hn.wang@gmail.com>
2024-04-08 10:18:58 -0700

    Used variables to manage query keys

--- src/components/features/findGroup/useJoinGroup.js
+++ src/components/features/findGroup/useJoinGroup.js
@@ -1,15 +1,19 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { joinGroupById } from "./findGroupHelper";
 import { Alert } from "react-native";
+import {
+  QUERY_KEY_GROUP_LIST,
+  QUERY_KEY_SEARCH_GROUP,
+} from "../../../utils/constants";
 
 export default function useJoinGroup() {
   const queryClient = useQueryClient();
   const { mutate, isPending } = useMutation({
     mutationFn: (groupId) => joinGroupById(groupId),
     onSuccess: (groupId) => {
-      queryClient.invalidateQueries({ queryKey: ["groups"] });
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY_GROUP_LIST] });
       queryClient.setQueriesData(
-        { queryKey: ["group-search-results"] },
+        { queryKey: [QUERY_KEY_SEARCH_GROUP] },
         (old) =>
           old?.map((group) =>
             group.groupId === groupId
--- src/components/features/studyGroup/useGetGroupDetail.js
+++ src/components/features/studyGroup/useGetGroupDetail.js
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getGroupDetail } from "./studyGroupHelper";
+import { QUERY_KEY_GROUP_DETAIL } from "../../../utils/constants";
 
 export default function useGetGroupDetail(groupId) {
   const { data, isLoading } = useQuery({
-    queryKey: [groupId, "group-detail"],
+    queryKey: [QUERY_KEY_GROUP_DETAIL, groupId],
     queryFn: () => getGroupDetail(groupId),
   });
   return { data, isLoading };
